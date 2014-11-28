64ef0057f98e6365e5d9ce6b8e5ae7cd41d7c457
Nikolay Mendyaev <newbiecraft@gmail.com>
2014-11-28 08:36:24 +0300

    v. 0.0.2 add substr search and fix some bugs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /**
  * Created by nikolaymendyaev on 26.11.14.
  */
-/**
- * Created by nikolaymendyaev on 26.11.14.
- */
 var E = {};
 
 function genSubstr(length){
@@ -13,43 +10,75 @@ function genSubstr(length){
 }
 
 /*
- @param array [{ key: 'url', val: {a lot of params} } ]
+ @param array [{ }]
  на уникальность нету никакой проверки так что при повторение ключа полностью будет скорее всего не сильно правильный вывод
  */
-E.HashIndex = function(array, hashFunction){
+E.HashIndex = function(array, keyFunction, hashFunction, isHashFunctionBasedOnKey){
+    this.isHashFunctionBasedOnKey = isHashFunctionBasedOnKey;
     if (typeof(hashFunction) == 'number')
         hashFunction = genSubstr(hashFunction);
     this.hashFunction = hashFunction;
+    this.keyFunction = keyFunction;
     this.index = array.reduce(function(index, elem){
-        var hash = hashFunction(elem.key);
-        if (index[hash])
-            index[hash].push(elem);
+        var key = keyFunction(elem);
+        if (isHashFunctionBasedOnKey)
+            var hash = hashFunction(key);
         else
-            index[hash] = [elem];
+            var hash = hashFunction(elem);
+        var obj = {
+            key: key,
+            val: elem
+        };
+        if (index[hash])
+            index[hash].push(obj);
+        else
+            index[hash] = [obj];
         return index;
     }, {})
 };
 
 E.HashIndex.prototype.searchByElem = function(elem){
-    var hash = this.hashFunction(elem.key);
-    return this.search(hash, elem.key);
+    var hash = this.hashFunction(elem);
+    var key = this.keyFunction(elem);
+    return this.search(hash, key);
 
 };
 
-E.HashIndex.prototype.search = function(hash, key){
+E.HashIndex.prototype.search = function(hash, key, substr){
     if (this.index[hash]){
-        for(var i = 0; i<this.index[hash].length; i++)
+        if (substr)
         {
-            if (this.index[hash][i].key == key)
-                return this.index[hash][i].val;
+            for(var i = 0; i<this.index[hash].length; i++)
+            {
+                if (key.indexOf(this.index[hash][i].key)>=0)
+                    return this.index[hash][i].val;
+            }
+        }
+        else
+        {
+            for(var i = 0; i<this.index[hash].length; i++)
+            {
+                if (this.index[hash][i].key == key)
+                    return this.index[hash][i].val;
+            }
         }
     }
     return null;
 };
 
+//don't work if hash not based on key
 E.HashIndex.prototype.searchByKey = function(key){
+    if (!this.isHashFunctionBasedOnKey)
+        return false;
     var hash = this.hashFunction(key);
     return this.search(hash, key);
+};
+//don't work if hash not based on key
+E.HashIndex.prototype.searchByKeySubstr = function(key){
+    if (!this.isHashFunctionBasedOnKey)
+        return false;
+    var hash = this.hashFunction(key);
+    return this.search(hash, key, true);
 };
 
 module.exports = E;
